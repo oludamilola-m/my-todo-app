e7e96a081c9e28f3234ccdd7b9d06cc5e6e41bef
Oludamilola Osiname <osinameoludamilola@gmail.com>
2021-03-17 22:26:41 +0100

    add todo

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import TextField from "./components/TextField";
 import store from "./redux/store";
+import axios from "axios";
 
 const App = () => {
+  const addTodo = (content) => {
+    return async (dispatch, _getState) => {
+      const res = await axios.post("http://localhost:3001/todos", {
+        content: content,
+        completed: false,
+      });
+      dispatch({
+        type: "ADD_TODO",
+        payload: res.data,
+      });
+    };
+  };
+
   const onKeyDown = (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      console.log(event.target.value);
-      // store.dispatch({ type: ADD_TODO, content: event.target.value });
-      // setTodos((prev) => [
-      //   { content: event.target.value, completed: false },
-      //   ...prev,
-      // ]);
+      store.dispatch(addTodo(event.target.value));
       event.target.value = "";
     }
   };
